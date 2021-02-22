e4a6558c1d0332682c6ab78ba6f3f5d30581e115
Maicon Silva <maiconrs95@gmail.com>
2021-02-22 18:58:41 -0300

    first commit

--- pages/index.js
+++ pages/index.js
@@ -52,12 +52,11 @@ export default function Home() {
 
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://www.maiconsilva.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+          Powered by @MaiconSilva
         </a>
       </footer>
     </div>
